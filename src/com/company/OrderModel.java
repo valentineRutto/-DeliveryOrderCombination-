d8cfbd3db163bd32a2e6fcb39d2bfd2e8aca47b5
valentinerutto <rutto28@gmail.com>
2018-06-11 10:59:45 +0300

    created order model

---
+++ src/com/company/OrderModel.java
@@ -0,0 +1,47 @@
+package com.company;
+
+public class OrderModel {
+
+    private String name;
+
+    private String pickUpLocation;
+
+    private String dropOffLocation;
+
+
+    public OrderModel(String the_name, String pick_at, String drop_at) {
+        this.name = the_name;
+        this.pickUpLocation = pick_at;
+        this.dropOffLocation = drop_at;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPickLocation() {
+        return pickUpLocation;
+    }
+
+    public void setPickLocation(String pickLocation) {
+        this.pickUpLocation = pickLocation;
+    }
+
+    public String getDropLocation() {
+        return dropOffLocation;
+    }
+
+    public void setDropLocation(String dropLocation) {
+        this.dropOffLocation = dropLocation;
+    }
+
+    @Override
+    public String toString() {
+        return "Order " + name + ": From " + pickUpLocation + " TO " + dropOffLocation;
+    }
+
+}
