2bd5f96a43647c5d2abad57f3460bc2554fd596a
valentinerutto <rutto28@gmail.com>
2018-07-10 14:51:42 +0300

    added permutation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,17 @@
 package com.company;
 
+import static com.company.Combination.combination;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        OrderCombination combination = new OrderCombination();
-        combination.processCombination();
+//        OrderCombination combination = new OrderCombination();
+//        combination.processCombination();
+
+
+        Object[] elements = new Object[] {'A','B','C','D','E', 'F', 'G', 'H'};
+
+        combination(elements,7);
     }
 }
